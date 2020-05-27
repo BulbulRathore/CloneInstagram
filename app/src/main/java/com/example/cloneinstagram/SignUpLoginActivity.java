99d8a0792d6510e22a1b203118183bbd141724b7
Bulbul Rathore <bulbulrathore0707@gmail.com>
2020-05-27 12:17:20 +0530

    Sign up and login Parse server with new Activity

--- app/src/main/java/com/example/cloneinstagram/SignUpLoginActivity.java
+++ app/src/main/java/com/example/cloneinstagram/SignUpLoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.cloneinstagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,6 +45,8 @@ public class SignUpLoginActivity extends AppCompatActivity {
                    public void done(ParseException e) {
                        if(e == null){
                            FancyToast.makeText(SignUpLoginActivity.this,appUser.get("username")+" is signed up successfully!!", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                           Intent intent = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                           startActivity(intent);
                        } else{
                            FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
                        }
@@ -60,6 +63,8 @@ public class SignUpLoginActivity extends AppCompatActivity {
                    public void done(ParseUser user, ParseException e) {
                         if(user != null && e == null){
                             FancyToast.makeText(SignUpLoginActivity.this,user.get("username")+" is login successfully!!", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                            Intent intent = new Intent(SignUpLoginActivity.this, WelcomeActivity.class);
+                            startActivity(intent);
                         } else {
                             FancyToast.makeText(SignUpLoginActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
                         }
--- app/src/main/java/com/example/cloneinstagram/WelcomeActivity.java
+++ app/src/main/java/com/example/cloneinstagram/WelcomeActivity.java
@@ -3,12 +3,32 @@ package com.example.cloneinstagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button btnWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        btnWelcome = findViewById(R.id.btnWelcome);
+        btnWelcome.setText("Welcome " + ParseUser.getCurrentUser().get("username") );
+
+        findViewById(R.id.btnLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                finish();
+                FancyToast.makeText(WelcomeActivity.this,ParseUser.getCurrentUser().get("username")+" is successfully logout", Toast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+            }
+        });
     }
 }
